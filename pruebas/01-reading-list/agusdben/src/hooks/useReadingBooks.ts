398acc9c52e443c16a493ca2e89ed5044cb1a124
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-08-06 02:52:56 -0300

    useReadingBooks created

---
+++ pruebas/01-reading-list/agusdben/src/hooks/useReadingBooks.ts
@@ -0,0 +1,44 @@
+import { useContext } from 'react'
+import { removeBookFromStore, storeNewBook } from '@/services/reading.services'
+import { getBooksFiltered } from '@/services/filter.services'
+import useFilterBooks from './useFilterBooks'
+import { ReadingBooksContext } from '@/context/ReadingBooksContext'
+import { type Book } from '@/types/book'
+
+const useReadingBooks = () => {
+  const { readingBooks, error, loading, setReadingBooks } = useContext(ReadingBooksContext)
+  const { filters } = useFilterBooks()
+
+  const addNewReadingBook = (book: Book) => {
+    (async () => {
+      setReadingBooks(await storeNewBook(book))
+    })()
+      .catch(() => {
+        console.error('Failed when add new book to read')
+      })
+  }
+
+  const removeReadingBook = (book: Book) => {
+    (async () => {
+      setReadingBooks(await removeBookFromStore(book))
+    })()
+      .catch(() => {
+        console.error('Failed when remove book to read')
+      })
+  }
+
+  const HandleRemoveAddReadingBook = (book: Book) => {
+    readingBooks.some(rb => rb.title === book.title)
+      ? removeReadingBook(book)
+      : addNewReadingBook(book)
+  }
+
+  return {
+    readingBooks: getBooksFiltered({ books: readingBooks, filters }),
+    error,
+    loading,
+    HandleRemoveAddReadingBook
+  }
+}
+
+export default useReadingBooks
