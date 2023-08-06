996995bb3b267d0b72aff34d4847b2679d5f68fb
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-08-06 02:49:17 -0300

    images domain added

--- pruebas/01-reading-list/agusdben/next.config.js
+++ pruebas/01-reading-list/agusdben/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+  images: {
+    domains: ['images-na.ssl-images-amazon.com']
+  }
+}
 module.exports = nextConfig
