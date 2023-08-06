1063d767284c8a49733444ceb48c14d90e05b802
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-08-06 02:54:54 -0300

    available filters and pages range added

---
+++ pruebas/01-reading-list/agusdben/src/constants/bookFilters.ts
@@ -0,0 +1,13 @@
+export const BOOK_FILTERS = {
+  'page range': 'page range',
+  genre: 'genre',
+  title: 'title'
+} as const
+
+export const PAGES_RANGE = {
+  all: 'all',
+  '0-199': '0-199',
+  '200-399': '200-399',
+  '400-599': '400-599',
+  '>600': '>600'
+}
